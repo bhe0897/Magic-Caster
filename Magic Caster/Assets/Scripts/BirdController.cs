1323f3db7482cc0280fedfebbc9342003085389a
winchell459 <johncmorris@gmail.com>
2020-08-23 14:13:52 -0700

    end of day 6

---
+++ Magic Caster/Assets/Scripts/BirdController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : EnemyController
+{
+    public float ThrustSpeed = 10;
+    public float ThrustRate = 1;
+    private float lastThrust = Mathf.NegativeInfinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(lastThrust + ThrustRate < Time.fixedTime)
+        {
+            //rb.AddForce(Vector2.up * ThrustSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, ThrustSpeed);
+            lastThrust = Time.fixedTime;
+        }
+    }
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TakeDamage(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Magic Caster/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyController : MonoBehaviour
+{
+    public float Health = 100;
+
+    public float WalkingSpeed = 5;
+
+    protected Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public abstract void TakeDamage(float damage);
+    public abstract void Die();
+}
